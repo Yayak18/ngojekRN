79eed87971e36c90af717e500d6249b0de0bfe7b
Nur Hidayatullah <nurhidayatullah.xviii@gmail.com>
2024-09-11 10:15:08 +0700

    cek error app not rendering

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text>Kotor Kah Kupingnya???</Text>
+      <Text>Ojeek Ojeeeek</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
